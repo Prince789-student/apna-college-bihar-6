248cfa117904fb5fce28bbd967334996249866bf
Prince <student@apnacollege.com>
2026-03-29 11:04:46 +0530

    auto-commit: automated code backup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+window.onload = () => { document.body.style.border = "10px solid blue"; document.body.innerHTML += '<h1 style="color:blue">SCRIPT EXECUTED</h1>'; };
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
