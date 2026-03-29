62c63fabe534f3752ad3aa8317f58866b81a4866
Prince <student@apnacollege.com>
2026-03-29 10:35:35 +0530

    feat: initialize React application with BrowserRouter in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,29 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
-  static getDerivedStateFromError(error) {
-    return { hasError: true, error };
-  }
-  render() {
-    if (this.state.hasError) {
-      return <div style={{padding: '50px', color: 'red'}}><h1>Something went wrong.</h1><pre>{this.state.error && this.state.error.toString()}</pre></div>;
-    }
-    return this.props.children; 
-  }
-}
-
-console.log("React Mounting Init...");
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
