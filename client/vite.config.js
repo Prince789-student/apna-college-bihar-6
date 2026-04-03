678fff33928fba20c4ad1ef42d59a7bed04a5cd7
Prince <student@apnacollege.com>
2026-04-03 11:56:11 +0530

    final: fixed vite.config.js syntax and direct outDir to server/public

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], build: { outDir: \"../server/public\", emptyOutDir: true },
+  plugins: [react()],
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': {
