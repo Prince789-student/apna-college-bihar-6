1f883054b8edf104aa94677c8f320ded5e188001
Prince <student@apnacollege.com>
2026-04-03 15:03:19 +0530

    manual build push

---
+++ server/public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'acb-cache-v2.1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
