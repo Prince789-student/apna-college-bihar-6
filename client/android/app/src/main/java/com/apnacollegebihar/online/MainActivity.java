1b652dbd0bf01e93c11ea61717d3f418aeff5827
Prince <student@apnacollege.com>
2026-05-18 11:30:07 +0530

    Fix Capacitor plugin registration order in MainActivity.java

--- client/android/app/src/main/java/com/apnacollegebihar/online/MainActivity.java
+++ client/android/app/src/main/java/com/apnacollegebihar/online/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         registerPlugin(AppBlockerPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }
