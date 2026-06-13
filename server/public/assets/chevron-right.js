18c2d3930660979cb34369904f86b6f9ad0d3860
Prince <student@apnacollege.com>
2026-06-13 12:40:32 +0530

    feat: simplify home page layout and force desktop view on mobile

--- server/public/assets/chevron-right.js
+++ server/public/assets/chevron-right.js
@@ -1,1 +1,1 @@
-import{m as t}from"./index.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
+import{l as t}from"./index.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
